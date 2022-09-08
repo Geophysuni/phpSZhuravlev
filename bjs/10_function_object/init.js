609d87694762659e19aa76c8531df900fda0e3d2
Журавлев Сергей Дмитриевич <szhuravlev@office.oldridgebarnes.ru>
2022-09-08 20:51:26 +0300

    person generator

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -3,5 +3,24 @@ window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.lastName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.YearOfBirth;
+    document.getElementById('proffesionOutput').innerText = initPerson.profession;
+    document.getElementById('fatherOutput').innerText = initPerson.father;
 };
+
+document.getElementById('generateBut').addEventListener('click', function () {
+    window.onload()
+})
+document.getElementById('clearBut').addEventListener('click', function () {
+    document.getElementById('firstNameOutput').innerText = 'Имя';
+    document.getElementById('genderOutput').innerText = 'Пол';
+    document.getElementById('birthYearOutput').innerText = 'Год рождения';
+    document.getElementById('surnameOutput').innerText = 'Фамилия';
+    document.getElementById('genderOutput').innerText = 'Пол';
+    document.getElementById('birthYearOutput').innerText = 'Год рождения';
+    document.getElementById('proffesionOutput').innerText = 'Профессия';
+    document.getElementById('fatherOutput').innerText = 'Отчество';
+})
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -36,6 +36,75 @@ const personGenerator = {
         }
     }`,
 
+    firstNameFemmaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Мария",
+            "id_2": "Оксана",
+            "id_3": "Марина",
+            "id_4": "Лариса",
+            "id_5": "Анастасия",
+            "id_6": "Екатерина",
+            "id_7": "Дарья",
+            "id_8": "Алена",
+            "id_9": "Любовь",
+            "id_10": "Валерия"
+        }
+    }`,
+
+    genderJson: `{
+        "count": 2,
+        "list": {     
+            "id_1": "Мужчина",
+            "id_2": "Женщина"
+        }
+    }`,
+
+    proffesonJson: `{
+        "count": 7,
+        "list": {     
+            "id_1": "Шахтер",
+            "id_2": "Солдат",
+            "id_3": "Слесарь",
+            "id_4": "Повар",
+            "id_5": "Строитель",
+            "id_6": "Учитель",
+            "id_7": "Программист"
+        }
+    }`,
+
+    fatherMaleJson: `{
+        "count": 7,
+        "list": {     
+            "id_1": "Петрович",
+            "id_2": "Игоревич",
+            "id_3": "Семенович",
+            "id_4": "Иванович",
+            "id_5": "Сергеевич",
+            "id_6": "Александрович",
+            "id_7": "Дмитриевич"
+        }
+    }`,
+
+    fatherFemaleJson: `{
+        "count": 7,
+        "list": {     
+            "id_1": "Петровна",
+            "id_2": "Игоревна",
+            "id_3": "Семеновна",
+            "id_4": "Ивановна",
+            "id_5": "Сергеевна",
+            "id_6": "Александровна",
+            "id_7": "Дмитриевна"
+        }
+    }`,
+
+    // randomGender: function() {
+
+    //     return this.randomValue(2)
+    //     // return this.randomValue(this.firstNameMaleJson);
+    // },
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
@@ -47,24 +116,76 @@ const personGenerator = {
         return obj.list[prop];
     },
 
+    randomFather: function(json) {
+
+        if (this.person.gender === 'Мужчина') {
+            return this.randomValue(this.fatherMaleJson)
+        } else {
+            return this.randomValue(this.fatherFemaleJson)
+        }
+        
+    },
+
+    randomProffesion: function(json) {
+
+        let tmpProf = this.randomValue(this.proffesonJson);
+
+        if (this.person.gender === 'Мужчина') {
+            return tmpProf
+        } else {
+            let skipFemProf = ["Шахтер", "Солдат", "Слесарь"];
+            while (skipFemProf.includes(tmpProf)){
+                tmpProf = this.randomValue(this.proffesonJson);
+            }
+            return tmpProf
+        }   
+        
+    },
+
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        if (this.person.gender === 'Мужчина') {
+            return this.randomValue(this.firstNameMaleJson)
+        } else {
+            return this.randomValue(this.firstNameFemmaleJson)
+        }  
+        // return this.randomValue(this.firstNameFemmaleJson)
     },
 
 
      randomSurname: function() {
-
+        if (this.person.gender === 'Женщина') {
+            return this.randomValue(this.surnameJson)+'a'}
         return this.randomValue(this.surnameJson);
-
     },
+
+    randomGender: function() {
+        return this.randomValue(this.genderJson);
+    },
+
+    // randomYearOfBirth: function() {
+    //     let year = Math.floor(Math.random() * (2004 - 1950) + 1950);
+    //     return year;
+    // },
+
+    randomDateOfBirth: function() {
+        let start = new Date(1950, 0, 1);
+        let end = new Date(2004, 0,1)
+        var options = {year: 'numeric', month: 'long', day: 'numeric' };
+        var randDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+        return randDate.toLocaleDateString('ru-RU', options);
+      },
 
 
     getPerson: function () {
         this.person = {};
         // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.lastName = this.randomSurname();
+        this.person.profession = this.randomProffesion()
+        this.person.YearOfBirth = this.randomDateOfBirth();
+        this.person.father = this.randomFather();
+        console.log(this.randomFather());
         return this.person;
     }
 };
